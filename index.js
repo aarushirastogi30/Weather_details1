60e90159de2c208a5011129abe81f65666fdd781
aarushirastogi30 <35369493+aarushirastogi30@users.noreply.github.com>
2018-01-12 18:02:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+
+if ("geolocation" in navigator)
+ {
+  $('.js-geolocation').show(); 
+} 
+else 
+{
+  $('.js-geolocation').hide();
+}
+
+
+
+/* Where are you? */
+
+$('.js-geolocation').on('click', function() 
+{
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); 
+//loading weather using your lat/lng coordinates
+  });
+});
+
+
+
+
+function loadWeather(location, woeid)
+ {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) 
+{
+      html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+          html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
